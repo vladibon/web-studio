224a27dbe56a47dbdde19924b960844a9292685f
Vladimir Bondar <vladi.bondar@gmail.com>
2021-05-22 22:00:06 +0300

    Fix mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-btn]");
   const mobileMenuRef = document.querySelector("[data-menu]");
+  const page = document.querySelector("[data-menu-page]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -9,5 +10,6 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("mobile-menu--is-open");
+    page.classList.toggle("page--menu-open");
   });
 })();
